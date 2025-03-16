a948a8f46aa7989edbbeed52013b84e5abbcf69c
Darius  Ramavarapu <ibexradius@gmail.com>
2025-03-16 17:05:16 -0400

    CCS-38: adds placeholder login page with proper state config and event handling

--- src/components/LoginField.js
+++ src/components/LoginField.js
@@ -4,19 +4,36 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export default function BasicButtons() {
+
+  const credentials = ['username', 'passsword']
+
+  const [submittedUsername, setSubmittedUsername] = React.useState('')
+  const [submittedPassword, setSubmittedPassword] = React.useState('')
+
+
+  handleLoginButtonClick = () => {
+    if (submittedPassword === credentials[0] && submittedUsername === credentials[1]) {
+      console.log('YOU HAVE LOGGED IN SUCCESSFULLY')
+    }
+  }
+
+
   return (
+    // add an onclick event for login button in JSX
     <Stack spacing={2}>
         <TextField
           username
           id="outlined-error"
           label="Username"
+          onChange={(e) => setSubmittedUsername(e.target.value)}
         />
         <TextField
           password
           id="outlined-error-helper-text"
           label="Password"
+          onChange={(e) => setSubmittedPassword(e.target.value)}
         />
-        <Button variant="contained">Login</Button>
+        <Button onClick={handleLoginButtonClick()} variant="contained">Login</Button>
     </Stack>
   );
 }
